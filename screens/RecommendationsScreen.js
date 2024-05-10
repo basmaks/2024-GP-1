485f5c35977de51ec1b0ffbf157c05bb338fe682
alanoudii11 <143386201+alanoudii11@users.noreply.github.com>
2024-05-10 19:02:19 +0300

    Update RecommendationsScreen.js

--- screens/RecommendationsScreen.js
+++ screens/RecommendationsScreen.js
@@ -1,97 +1,244 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, ScrollView } from 'react-native';
 import { StyleSheet, Image } from "react-native";
-import React from 'react';
-import { themeColors } from '../theme';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import TopNavBar from '../navigation/TopNavBar';
 import BottomNavBar from '../navigation/BottomNavBar';
+import { useNavigation } from '@react-navigation/native';
+import { themeColors } from '../theme';
+import * as Icons from 'react-native-heroicons/outline';
+import { auth} from '../config/firebase'
+import { doc, getDoc,getDocs, setDoc, addDoc, collection } from "firebase/firestore";
+import { db } from '../config/firebase';
+import { LineChart } from 'react-native-chart-kit';
+import Kwh_RealTimeChart from '../charts/Kwh_RealTimeChart';//1
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function RecommendationsScreen() {
-  return (
-    <View style={{ flex: 1 }}>
-      <TopNavBar />
-      <ScrollView contentContainerStyle={styles.scrollContent}>
-      <View style={styles.sugg}>
-        <Text style={styles.descriptionText}>اقتراحات</Text>
-      </View>
-      
-      
-        <View style={styles.recommendationsContent}>
-          <View style={styles.recommendationItem}>
-            <Text style={styles.recommendationText}>إغلاق أفياش الكهرباء يقلل ٢٠٪ من استهلاك الطاقة</Text>
-            <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
-          </View>
-          <View style={styles.recommendationItem}>
-            <Text style={styles.recommendationText}>إغلاق الأجهزة الإلكترونية عند عدم الاستخدام يمكن أن يوفر كميات كبيرة من الطاقة.</Text>
-            <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
-          </View>
-          <View style={styles.recommendationItem}>  
-            <Text style={styles.recommendationText}>إغلاق أفياش الكهرباء يقلل ٢٠٪ من استهلاك الطاقة</Text>
-            <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
-          </View>
-          <View style={styles.recommendationItem}>  
-            <Text style={styles.recommendationText}>إغلاق أفياش الكهرباء يقلل ٢٠٪ من استهلاك الطاقة</Text>
-            <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
-          </View>
-          <View style={styles.recommendationItem}>
-            <Text style={styles.recommendationText}>إغلاق أفياش الكهرباء يقلل ٢٠٪ من استهلاك الطاقة</Text>
-            <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
-          </View>
-          <View style={styles.recommendationItem}>
-            <Text style={styles.recommendationText}>إغلاق الأجهزة الإلكترونية عند عدم الاستخدام يمكن أن يوفر كميات كبيرة من الطاقة.</Text>
-            <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
-          </View>
-        </View>
-      </ScrollView>
 
-      <BottomNavBar />
+  const navigation = useNavigation();
+  const [userTips, setUserTips] = useState([]);
+  
+  const [dailyUsageExceeded, setDailyUsageExceeded] = useState(false);
+
+  useEffect(() => {
+    const fetchUserTips = async () => {
+      try {
+        const userTipsRef = collection(db, 'userTips', auth.currentUser.uid, 'Tips');
+        const querySnapshot = await getDocs(userTipsRef);
+        const userTipsData = querySnapshot.docs.map(doc => ({
+          tipId: doc.data().tipId,
+         
+        }));
+
+        const tipTitles = {};
+        const tipsRef = collection(db, 'recommendations');
+        const tipsSnapshot = await getDocs(tipsRef);
+        tipsSnapshot.forEach(doc => {
+          tipTitles[doc.id] = doc.data().tip;
+        });
+
+        const userTipsWithTitles = userTipsData.map(tip => ({
+          ...tip,
+          title: tipTitles[tip.tipId]
+        }));
+
+        setUserTips(userTipsWithTitles);
+      } catch (error) {
+        console.error('Error fetching user tips:', error);
+      }
+    };
+
+    async function sendPushNotification(expoPushToken) {
+      const message = {
+        to: expoPushToken,
+        sound: 'default',
+        title: 'Original Title',
+        body: 'And here is the body!',
+        data: { someData: 'goes here' },
+      };
+
+      await fetch('https://exp.host/--/api/v2/push/send', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Accept-encoding': 'gzip, deflate',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(message),
+      });
+    }
+
+    const checkDailyUsage = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/api/getRecentUsage');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+    
+        const jsonData = await response.json();
+    
+        let totalDailyUsage = 0;
+        for (const deviceId in jsonData) {
+          const device = jsonData[deviceId];
+          if (device && typeof device === 'object' && device.channels) {
+            for (const channelId in device.channels) {
+              const channel = device.channels[channelId];
+              if (channel && typeof channel === 'object' && !isNaN(channel.usage)) {
+                totalDailyUsage += Number(channel.usage);
+              }
+            }
+          }
+        }
+    
+        if (totalDailyUsage > 5) {
+          handleTip('tip01');
+        }
+        if (totalDailyUsage > 10) {
+          handleTip('tip02');
+        }
+        if (totalDailyUsage > 20) {
+          handleTip('tip03');
+        }
+        if (totalDailyUsage > 29) {
+          handleTip('tip04');
+        }
+        if (totalDailyUsage > 2) {
+          handleTip('tip05');
+        }
+        if (totalDailyUsage > 0.0000000776346206665039) {
+          handleTip('tip06');
+        }
+        if (totalDailyUsage > 76) {
+          handleTip('tip07');
+        }
+          if (totalDailyUsage > 0.000045) {
+            handleTip('tip08');
+        }
+
+        // Add more conditions for other tips as needed...
+      } catch (error) {
+        console.error('Error fetching real-time usage:', error);
+      }
+    };
+    
+    const handleTip = async (tipId) => {
+      try {
+        const userUid = auth.currentUser?.uid;
+        if (!userUid) {
+          console.error('No authenticated user available.');
+          return;
+        }
+    
+        const userDocRef = doc(db, 'notificationsdb', userUid);
+        const userDocSnapshot = await getDoc(userDocRef);
+    
+        if (!userDocSnapshot.exists()) {
+          console.error('User data not found.');
+          return;
+        }
+    
+        const userData = userDocSnapshot.data();
+        console.log('userData: ', userData);
+        const expoPushToken = userData.token_id;
+    
+        if (expoPushToken) {
+          await sendPushNotification(expoPushToken);
+          const userTipsRef = collection(db, 'userTips', userUid, 'Tips');
+          await addDoc(userTipsRef, {
+            tipId: tipId,
+            title: tipTitle, // Include the title field
+          });
+          console.log('Tip added successfully.');
+        } else {
+          console.error('No push token available.');
+        }
+      } catch (error) {
+        console.error('Error handling tip:', error);
+      }
+    };
+
+    fetchUserTips();
+    checkDailyUsage();
+
+    const intervalId = setInterval(() => {
+      fetchUserTips();
+      checkDailyUsage();
+    }, 5 * 60 * 1000);
+
+    return () => clearInterval(intervalId);
+
+  }, []);
+
+  return (
+    <View style={styles.container}>
+
+      <TopNavBar />
+
+      <ScrollView style={{ flex: 1 }}>
+
+      <View style={styles.tipsSection}>
+      <Text style={styles.header}>اقتراحات</Text>
+      {userTips.map(item => (
+  <View key={item.tipId} style={styles.tipItem}>
+    <View style={styles.contentContainer}>
+      <Text style={styles.tipTitle}>{item.title}</Text>
+      <Image source={require('../assets/icons/recommendations.png')} style={[styles.recommendationIcon, { tintColor: 'yellow' }]} />
     </View>
+  </View>
+))}
+
+      </View>
+      </ScrollView>
+      <BottomNavBar/>
+      </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  scrollContent: {
-    flexGrow: 1,
-    paddingBottom: 100, 
+  container: {
+    flex: 1,
+    //backgroundColor: themeColors.bg, // Set background color
   },
-  recommendationsContent: {
-    marginTop: 20,
-    marginHorizontal: 20,
+  tipsSection:
+  {
+    margin: 20,
   },
-  sugg: {
-    alignItems: 'flex-end', 
-    paddingTop: 20,
-    marginHorizontal: 20,
+  header: {
+    fontSize: 20, // Adjust font size
+    fontWeight: 'bold',
+    color: '#143638',
+    textAlign: 'right', // Align text to the right
+    marginBottom: 20,
   },
-  recommendationItem: {
+  tipItem: {
     backgroundColor: themeColors.bg, 
-    padding: 10,
-    borderRadius: 15,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    paddingHorizontal: 20, 
-    marginBottom: 35,
-    alignItems: 'center',
+    padding: 20,
+    marginBottom: 10,
+    borderRadius: 15, // Adjust border radius
+    //textAlign: 'right', // Align text to the right
     
+  },
+  tipTitle: {
+    fontSize: 16, // Adjust font size
+    textAlign: 'right', // Align text to the right
+    color: '#FFFFFF',
+   // marginRight: 10,
+   //backgroundColor: 'blue', // Set background color
+   flex: 1, // Allow the title to expand and fill available space
+  },
+  contentContainer: {
+    //alignItems: 'center',
+    //backgroundColor: 'pink', // Set background color
+    flexDirection: 'row',
+
   },
   recommendationIcon: {
     width: 30,
     height: 30,
-    marginLeft: 5,
-    
-  },
-  recommendationText: {
-    fontSize: 16,
-    //fontWeight: 'bold',
-    color: '#FFFFFF',
-    textAlign: 'right',
-    padding: 7,
-    marginLeft: 20,
-  },
-  descriptionText: {
-    fontSize: 20,
-    color: '#143638', 
-    textAlign: 'right',
-    fontWeight: 'bold',
+    marginLeft: 10,
 
+    
   },
 });
