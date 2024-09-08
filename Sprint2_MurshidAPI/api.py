21b7f694b78327d2c5f2ce6b85a4371e36b7d8b9
“basmaks” <basmaalsulaim@hotmail.com>
2024-09-08 09:41:10 +0300

    Add background task for real-time fetching

--- Sprint2_MurshidAPI/api.py
+++ Sprint2_MurshidAPI/api.py
@@ -2,14 +2,8 @@ import os
 import json
 from flask import Flask, jsonify
 from flask_cors import CORS
-import pyemvue
-from pyemvue.enums import Scale, Unit
-from datetime import datetime
-import pytz
 import firebase_admin
 from firebase_admin import credentials, firestore, initialize_app
-import time  # Add this for the delay between requests
-from config import EMVUE_EMAIL, EMVUE_PASSWORD
 
 # Load credentials from environment variable
 cred_json = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
@@ -26,34 +20,20 @@ db = firestore.client()
 app = Flask(__name__)
 CORS(app)
 
-def fetch_energy_usage():
-    vue = pyemvue.PyEmVue()
-    vue.login(username=EMVUE_EMAIL, password=EMVUE_PASSWORD)
-    devices = vue.get_devices()
-
-    for device in devices:
-        usage = vue.get_device_list_usage(deviceGids=[device.device_gid], instant=None, scale=Scale.SECOND.value, unit=Unit.KWH.value)
-        channels_data = usage[device.device_gid].channels
-        total_channels_usage_kWh = sum(channel.usage for channel in channels_data.values())
-        
-        timestamp = datetime.now(pytz.timezone('Asia/Riyadh'))
-        usage_data = {
-            'total_channels_usage_kWh': total_channels_usage_kWh,
-            'userId': device.device_gid,
-            'timestamp': timestamp,
-            'device_name': usage[device.device_gid].device_name,
-            'channels': {channel_num: {'name': channel.name, 'usage': channel.usage} for channel_num, channel in channels_data.items()},
-        }
-
-        # Save to Firestore
-        db.collection('electricity_usage').add(usage_data)
-
 @app.route('/', methods=['GET'])
 def index():
     return "Welcome to the API", 200
 
+@app.route('/api/getRecentUsage', methods=['GET'])
+def get_recent_usage():
+    # Query the Firestore database to get the most recent usage data
+    docs = db.collection('electricity_usage').order_by('timestamp', direction=firebase_admin.firestore.Query.DESCENDING).limit(1).stream()
+    usage_data = [doc.to_dict() for doc in docs]
+
+    if usage_data:
+        return jsonify(usage_data), 200
+    else:
+        return jsonify({"message": "No data found"}), 404
+
 if __name__ == '__main__':
-    # Infinite loop to run automatically
-    while True:
-        fetch_energy_usage()
-        time.sleep(1)  # Wait for 60 seconds before fetching data again
+    app.run()
