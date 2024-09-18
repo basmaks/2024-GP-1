28a07c694438183b25e67a597b1479239bcacc4e
alanoudii11 <143386201+alanoudii11@users.noreply.github.com>
2024-09-18 20:56:03 +0300

    Add files via upload

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -12,13 +12,31 @@ import RecommendationsScreen from '../screens/RecommendationsScreen';
 import AnalyticsScreen from '../screens/AnalyticsScreen';
 import AlertsScreen from '../screens/AlertsScreen';
 import EditProfileScreen from '../screens/EditProfileScreen';
-import ConnectScreen from '../screens/ConnectScreen';
+import ConnectScreen from '../screens/connectionSteps/ConnectScreen';
 import ConnectSteps from '../screens/ConnectSteps';
+import ContactUsScreen from '../screens/ContactUsScreen';
 import ForgetPasswordScreen from '../screens/ForgetPasswordScreen';
 import ChangePasswordScreen from '../screens/ChangePasswordScreen';
 import DeviceConnectionScreen from '../screens/DeviceConnectionScreen';
-import ContactUsScreen from '../screens/ContactUsScreen';
+import Support from '../screens/connectionSteps/Support';
 import OutletsScreen from '../screens/OutletsScreen';
+import ConStep0 from '../screens/connectionSteps/ConStep0';
+import ConStep1 from '../screens/connectionSteps/ConStep1';
+import ConStep2 from '../screens/connectionSteps/ConStep2';
+import ConStep3 from '../screens/connectionSteps/ConStep3';
+import ConStep4 from '../screens/connectionSteps/ConStep4';
+import ConStep5 from '../screens/connectionSteps/ConStep5';
+import ConStep6 from '../screens/connectionSteps/ConStep6';
+import ConStep7 from '../screens/connectionSteps/ConStep7';
+import ConStep8 from '../screens/connectionSteps/ConStep8';
+import ConStep9 from '../screens/connectionSteps/ConStep9';
+import ConStep10 from '../screens/connectionSteps/ConStep10';
+import ConStep11 from '../screens/connectionSteps/ConStep11';
+import ConStep12 from '../screens/connectionSteps/ConStep12';
+import ConStep13 from '../screens/connectionSteps/ConStep13';
+import ConStep14 from '../screens/connectionSteps/ConStep14';
+import ConStep15 from '../screens/connectionSteps/ConStep15';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -45,6 +63,26 @@ export default function AppNavigation() {
           <Stack.Screen name="DeviceConnection" options={{headerShown: false}} component={DeviceConnectionScreen}/>
           <Stack.Screen name="ContactUs" options={{headerShown: false}} component={ContactUsScreen}/>
           <Stack.Screen name="Outlets" options={{headerShown: false}} component={OutletsScreen}/>
+          <Stack.Screen name="Step0" options={{headerShown: false}} component={ConStep0}/>
+          <Stack.Screen name="Step1" options={{headerShown: false}} component={ConStep1}/>
+          <Stack.Screen name="Step2" options={{headerShown: false}} component={ConStep2}/>
+          <Stack.Screen name="Step3" options={{headerShown: false}} component={ConStep3}/>
+          <Stack.Screen name="Step4" options={{headerShown: false}} component={ConStep4}/>
+          <Stack.Screen name="Step5" options={{headerShown: false}} component={ConStep5}/>
+          <Stack.Screen name="Step6" options={{headerShown: false}} component={ConStep6}/>
+          <Stack.Screen name="Step7" options={{headerShown: false}} component={ConStep7}/>
+          <Stack.Screen name="Step8" options={{headerShown: false}} component={ConStep8}/>
+          <Stack.Screen name="Step9" options={{headerShown: false}} component={ConStep9}/>
+          <Stack.Screen name="Step10" options={{headerShown: false}} component={ConStep10}/>
+          <Stack.Screen name="Step11" options={{headerShown: false}} component={ConStep11}/>
+          <Stack.Screen name="Step12" options={{headerShown: false}} component={ConStep12}/>
+          <Stack.Screen name="Step13" options={{headerShown: false}} component={ConStep13}/>
+          <Stack.Screen name="Step14" options={{headerShown: false}} component={ConStep14}/>
+          <Stack.Screen name="Step15" options={{headerShown: false}} component={ConStep15}/>
+          <Stack.Screen name="Support" options={{headerShown: false}} component={Support} />
+
+  
+
         </Stack.Navigator>
       </NavigationContainer>
     )
